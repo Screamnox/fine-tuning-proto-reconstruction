d2dd08b140532c190efb53a94bd091de82986d46
Screamnox <92693756+Screamnox@users.noreply.github.com>
2023-03-06 20:58:34 +0100

    🐛🎨 Fix batch error
    Details : Due to improper slicing of the data and then inappropriate processing, the batches turned out to be incorrect.
    
    Now: there is still the memory problem in the first line `loss = outputs.loss`.

--- test.py
+++ test.py
@@ -1,116 +1,147 @@
 import torch
 from torch.utils.data import DataLoader
-from transformers import MBartTokenizer, MBartForConditionalGeneration, DataCollatorWithPadding, AdamW, get_scheduler
+from transformers import MBartTokenizer, MBartForConditionalGeneration, DataCollatorForSeq2Seq, AdamW, get_scheduler
 from datasets import load_dataset
 from tqdm.auto import tqdm
 
 """--SETTINGS--"""
-
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
 checkpoint = "facebook/mbart-large-cc25"
-tokenizer = MBartTokenizer.from_pretrained(checkpoint)
+tokenizer = MBartTokenizer.from_pretrained(checkpoint, return_tensors="pt")
 model = MBartForConditionalGeneration.from_pretrained(checkpoint)
 model.to(device)
 
+print("The model and the tokenizer are initialized.")
+#---
+
 """--DATA PROCESSING--"""
+train_size = 0.8
+seed = 42
+max_length = 32
+batch_size = 4
 
 # As a test to make sure I'm doing it right, 
 # I'm using a modified database of the real Latin Reconstruction database of AB Antiquo.
 # Obviously for true fine-tuning I would use the entire database and the tokenize function will be modified.
 datasets = load_dataset("csv", data_files="Latin_reconstruction_database - Copy.csv", sep=",")
+split_datasets = datasets["train"].train_test_split(train_size=train_size, seed=seed)
+split_datasets["validation"] = split_datasets.pop("test")
 
-def tokenize_function(example):
-    inputs = tokenizer(example['ITALIAN'], truncation=True)     # padding=True, max_length=32 ?
-    targets = tokenizer(example['LATIN-CORRECT'], truncation=True)      # padding=True, max_length=32 ?
-    return {
-        'input_ids': inputs['input_ids'],
-        'attention_mask': inputs['attention_mask'],
-        'decoder_input_ids': targets['input_ids'],
-        'decoder_attention_mask': targets['attention_mask'],
-        'labels': targets['input_ids']
-    }
+print("Recovery and separation of data performed.")
+#---
 
-tokenized_datasets = datasets.map(tokenize_function, batched=True)
-tokenized_datasets = tokenized_datasets.remove_columns(["ITALIAN", "LATIN-CORRECT"])
-# Maybe instead of doing a dynamic padding, we could do a padding in the tokenize_function and set a maximum length.
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+def preprocess_function(examples):
+    inputs = [ex for ex in examples["ITALIAN"]]
+    targets = [ex for ex in examples["LATIN-CORRECT"]]
+    model_inputs = tokenizer(
+        inputs,
+        #padding="max_length",
+        truncation=True,
+        max_length=max_length
+    )
+    labels = tokenizer(
+        targets,
+        #padding="max_length",
+        truncation=True,
+        max_length=max_length
+    )
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+tokenized_datasets = split_datasets.map(
+    preprocess_function,
+    batched=True,
+    remove_columns=split_datasets["train"].column_names,
+)
+
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model, return_tensors="pt")
 tokenized_datasets.set_format("torch")
-#TODO: Save tokenized_datasets to avoid retokenizing it each time
 
-# Instead of using sklearn or others, I manually split the database. 
-train_dataset = tokenized_datasets['train'].shuffle(seed=42).select(range(3000))
-val_dataset = tokenized_datasets['train'].shuffle(seed=42).select(range(3000, 3500))
-test_dataset = tokenized_datasets['train'].shuffle(seed=42).select(range(3500, 3967))
+print("`tokenized_datasets` is set.")
+#---
 
-batch_size = 8
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=data_collator)
-val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=data_collator)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=data_collator)
+train_dataloader = DataLoader(
+    tokenized_datasets["train"],
+    shuffle=True,
+    collate_fn=data_collator,
+    batch_size=batch_size,
+)
+eval_dataloader = DataLoader(
+    tokenized_datasets["validation"],
+    shuffle=False,
+    collate_fn=data_collator,
+    batch_size=batch_size
+)
 
-#TODO: Correct batch error !
-# I guess it comes from a structuring problem.
-"""
+print("The DataLoaders are initialized.")
+#---
+
+# Check that there are no errors in the data processing.
 for batch in train_dataloader:
     break
 {k: v.shape for k, v in batch.items()}
-"""
+
+print("The batches in the `train_dataloader` are correct.")
+#---
 
 """--TRAINING--"""
-# I preferred to create a training without using `Trainer class`.
-
-num_epochs = 10
+print("Setting up the training")
+#---
 
 optimizer = AdamW(model.parameters(), lr=1e-5)
-# loss_fn = torch.nn.CrossEntropyLoss()
 
-num_training_steps = num_epochs * len(train_dataloader)
+num_train_epochs = 10
+num_update_steps_per_epoch = len(train_dataloader)
+num_training_steps = num_train_epochs * num_update_steps_per_epoch
+
 lr_scheduler = get_scheduler(
     "linear",
     optimizer=optimizer,
-    num_warmup_steps=500,
+    num_warmup_steps=0,
     num_training_steps=num_training_steps,
 )
 
+print("Start of the training.")
+#---
+
 progress_bar = tqdm(range(num_training_steps))
 
-model.train()
-for epoch in range(num_epochs):
+for epoch in range(num_train_epochs):
+    # Training
+    model.train()
     for batch in train_dataloader:
         batch = {k: v.to(device) for k, v in batch.items()}
         outputs = model(**batch)
         loss = outputs.loss
-        # loss = loss_fn(outputs.logits, labels)
         loss.backward()
 
         optimizer.step()
         lr_scheduler.step()
         optimizer.zero_grad()
         progress_bar.update(1)
-"""TODO:
-        # running_loss += loss.item()
-        # preds = outputs.logits.argmax(dim=1)
-        # correct_predictions += torch.sum(preds == labels)
 
-    # epoch_loss = running_loss / len(train_dataloader)
-    # epoch_acc = correct_predictions / len(train_data)
+    # Evaluation
+    model.eval()
+    total_loss = 0
+    total_batches = 0
+    for batch in tqdm(eval_dataloader):
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+            loss = outputs.loss
+            total_loss += loss.item()
+            total_batches += 1
+    
+    avg_loss = total_loss / total_batches
 
-    # print(f"Epoch {epoch+1}/{epochs}, Loss: {epoch_loss:.4f}, Accuracy: {epoch_acc:.4f}")
-"""
+    print(f"epoch {epoch}, loss: {avg_loss:.2f}")
 
-"""TODO:
-model.eval()
-for batch in eval_dataloader:
-    batch = {k: v.to(device) for k, v in batch.items()}
-    with torch.no_grad():
-        outputs = model(**batch)
+    # Save
+    model.save_pretrained(f"./output_dir/epoch_{epoch}")
+    tokenizer.save_pretrained(f"./output_dir/epoch_{epoch}")
+    progress_bar.refresh()
 
-    logits = outputs.logits
-    predictions = torch.argmax(logits, dim=-1)
+progress_bar.close()
 
-    # test_loss = test_running_loss / len(test_dataloader)
-    # test_acc = test_correct_predictions / len(test_data)
-
-    # print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_acc:.4f}")
-"""
-
-model.save_pretrained("trained_model")
+print("The training has ended.\n The models have been saved between each `epoch`.")
